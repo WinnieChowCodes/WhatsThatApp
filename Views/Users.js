46ea46a8c9dd03f078bf666025be1a5454fe676c
Winnie Chow <WinnieChow606@outlook.com>
2023-02-21 10:36:49 +0000

    Add files via upload

---
+++ Views/Users.js
@@ -0,0 +1,24 @@
+import React, { Component, StrictMode } from 'react';
+import { Text, TextInput, View, Button, StyleSheet, Image } from 'react-native';
+
+class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+  styles = StyleSheet.create({
+
+
+  })
+
+  render() {
+    return (
+      <View>
+        <Text>This is the Users View</Text>
+      </View>
+    );
+  }
+}
+
+export default Profile
