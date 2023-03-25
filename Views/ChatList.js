eedd0dd79f4575429572e8d47db61f8316f0691a
Winnie Chow <WinnieChow606@outlook.com>
2023-03-25 21:11:50 +0000

    Update ChatList.js
    Modified code in accordance to Eslint guidance

--- Views/ChatList.js
+++ Views/ChatList.js
@@ -1,41 +1,75 @@
-import React, { Component, StrictMode } from 'react';
-import { Text, TextInput, View, Button, StyleSheet, Image, Modal } from 'react-native';
-import { ActivityIndicator, FlatList, Touchable, TouchableOpacity } from 'react-native-web';
+/* eslint-disable class-methods-use-this */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
+import React, { Component } from 'react';
+import {
+  Text, View, StyleSheet,
+} from 'react-native';
+import {
+  FlatList, TouchableOpacity,
+} from 'react-native-web';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import ModalUtil from './utils/Modal';
 
 class ChatList extends Component {
+  styles = StyleSheet.create({
+    header: {
+      backgroundColor: '#3a75b5',
+      padding: 10,
+      color: 'white',
+      fontWeight: 'bold',
+      fontSize: 20,
+    },
+    chatStyle: {
+      backgroundColor: '#d7dce0',
+      margin: 10,
+    },
+    chatTitle: {
+      fontSize: 20,
+      fontWeight: 'bold',
+    },
+    buttonStyle: {
+      backgroundColor: '#3a75b5',
+      padding: 10,
+      width: 100,
+    },
+
+  });
+
   constructor(props) {
     super(props);
 
     this.state = {
-      query: "",
-      isLoading: true,
       chatsListData: [],
-      newChatName: "",
-      modalVisible: false
-    }
+    };
   }
 
+  // Executes as soon as the component renders
   async componentDidMount() {
-    this.getData();
-    await AsyncStorage.removeItem("chatID");
-  };
+    this.unsubscribe = this.props.navigation.addListener('focus', async () => {
+      this.getData();
+      await AsyncStorage.removeItem('chatID');
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
 
   async getData() {
-    return fetch("http://localhost:3333/api/1.0.0/chat", {
+    return fetch('http://localhost:3333/api/1.0.0/chat', {
       method: 'get',
       headers: {
-        'x-authorization': await AsyncStorage.getItem("SessionToken")
-      }
-    }) 
-    .then((response) => response.json())
+        'x-authorization': await AsyncStorage.getItem('SessionToken'),
+      },
+    })
+      .then((response) => response.json())
       .then((responseJson) => {
+        console.log(responseJson);
         this.setState({
-          isLoading: false,
-          chatsListData: responseJson
-        })
+          chatsListData: responseJson,
+        });
       })
       .catch((error) => {
         console.log(error);
@@ -43,69 +77,59 @@ class ChatList extends Component {
   }
 
   async getSingleChatHandler(chatID) {
-    try{
-      await AsyncStorage.setItem("chatID", chatID)
-      this.props.navigation.navigate('Chat')
-
-    }catch{
-      throw "Something went wrong!"
+    try {
+      await AsyncStorage.setItem('chatID', chatID);
+      this.props.navigation.navigate('Chat');
+    } catch {
+      return 'Something went wrong!';
     }
+    return null;
   }
 
-  styles = StyleSheet.create({
-    header:{
-      backgroundColor: '#3a75b5',
-      padding: 10,
-      color: 'white',
-      fontWeight: 'bold',
-      fontSize: 20
-    },
-    chatStyle: {
-      backgroundColor: '#d7dce0',
-      margin: 10
-    },
-    chatTitle: {
-      fontSize: 20,
-      fontWeight: 'bold'
-    },
-    buttonStyle:{
-      backgroundColor: '#3a75b5',
-      padding: 10,
-      width: 100
+  determineLastMessage = (lastMessage) => {
+    if (lastMessage.message === undefined) {
+      return 'No Messages!';
     }
 
-  })
+    return `${lastMessage.author.first_name}: ${lastMessage.message}`;
+  };
 
   render() {
-    if (this.state.chatsListData.length == 0) {
+    if (this.state.chatsListData.length === 0) {
       return (
         <View>
           <Text>No Current Chats! Start a new chat</Text>
-          <ModalUtil/>
+          <ModalUtil />
         </View>
-      )
+      );
     }
-    else {
-      return (
-        <View>
-          <FlatList
-            data={this.state.chatsListData}
-            renderItem={({ item }) => (
-              <View>
-                <br />
-                <TouchableOpacity style={this.styles.chatStyle} onPress={() => this.getSingleChatHandler(item.chat_id)}>
-                  <Text style={this.styles.chatTitle}>{item.name}</Text>
-                  <Text>Created By: {item.creator.first_name}</Text>
-                  <Text>Last Message: </Text>
-                </TouchableOpacity>
-              </View>
-            )}
-            keyExtractor={({ user_id }, index) => user_id} />
-            <ModalUtil/>
-        </View>
-      )
-    }
+
+    return (
+      <View>
+        <ModalUtil />
+        <FlatList
+          data={this.state.chatsListData}
+          renderItem={({ item }) => (
+            <View>
+              <br />
+              <TouchableOpacity
+                style={this.styles.chatStyle}
+                onPress={() => this.getSingleChatHandler(item.chat_id)}
+              >
+                <Text style={this.styles.chatTitle}>{item.name}</Text>
+                <Text>
+                  Created By:
+                  {item.creator.first_name}
+                </Text>
+                <Text>{this.determineLastMessage(item.last_message)}</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+          keyExtractor={({ userId }) => userId}
+        />
+      </View>
+    );
   }
 }
 
-export default ChatList
+export default ChatList;
