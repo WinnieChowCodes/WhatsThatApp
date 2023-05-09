eeadd5170113024e1083c53e9654f31442f3a137
Winnie Chow <WinnieChow606@outlook.com>
2023-05-09 17:25:19 +0100

    Update Contacts.js
    Ammended Styling
    Added Response code Handling

--- Views/Contacts.js
+++ Views/Contacts.js
@@ -1,175 +1,227 @@
-import React, { Component, StrictMode } from 'react';
-import { Text, TextInput, View, Button, StyleSheet, Image } from 'react-native';
-import { ActivityIndicator, FlatList, Touchable, TouchableOpacity } from 'react-native-web';
+/* eslint-disable consistent-return */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/no-unused-state */
+import React, { Component } from 'react';
+import {
+  Text, View,
+} from 'react-native';
+import {
+  ActivityIndicator, FlatList, TouchableOpacity,
+} from 'react-native-web';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { ListItem } from '@rneui/themed';
+import { Avatar } from '@rneui/base';
+
 class Contacts extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      query: "",
       isLoading: true,
       contactsListData: [],
       deleteContactID: 0,
       blockContactID: 0,
-      errorMessage: ""
-    }
+      errorMessage: '',
+    };
   }
 
-  componentDidMount() {
-    this.getData();
-  };
+  // Executes as soon as the component renders
+  async componentDidMount() {
+    this.unsubscribe = this.props.navigation.addListener('focus', async () => {
+      this.getData();
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
 
   async getData() {
-    return fetch("http://localhost:3333/api/1.0.0/contacts", {
+    return fetch('http://localhost:3333/api/1.0.0/contacts', {
       method: 'get',
       headers: {
-        'x-authorization': await AsyncStorage.getItem("SessionToken")
-      }
+        'x-authorization': await AsyncStorage.getItem('SessionToken'),
+      },
     })
-    .then((response) => response.json())
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        }
+        if (response.status === 401) {
+          // User is unauthorised - return to login screen
+          this.props.navigation.navigate('Login');
+        } else {
+          const err = 'Server Error! Please try again later!';
+          throw err;
+        }
+      })
       .then((responseJson) => {
         this.setState({
           isLoading: false,
-          contactsListData: responseJson
-        })
+          contactsListData: responseJson,
+        });
       })
       .catch((error) => {
-        console.log(error);
-      });
-  }
-
-  async deleteContact(){
-    return fetch("http://localhost:3333/api/1.0.0/user/"+this.state.deleteContactID+"/contact", {
-      method: 'delete',
-      headers: {
-        'x-authorization': await AsyncStorage.getItem("SessionToken")
-      }
-    })
-      .then(() => {
-        this.setState({
-          isLoading: false,
-          errorMessage: "Contact Removed"
-        })
-        console.log("Contact Removed")
-        this.getData();
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-
-  async blockContact(){
-    return fetch("http://localhost:3333/api/1.0.0/user/"+this.state.blockContactID+"/block", {
-      method: 'post',
-      headers: {
-        'x-authorization': await AsyncStorage.getItem("SessionToken")
-      }
-    })
-      .then(() => {
-        this.setState({
-          isLoading: false,
-          errorMessage: "Contact Blocked"
-        })
-        console.log("Contact Blocked")
-        this.getData();
-      })
-      .catch((error) => {
-        console.log(error);
+        this.setState({ errorMessage: error });
       });
   }
 
   deleteContactHandler = (ID) => {
     this.setState({
       isLoading: true,
-      deleteContactID: ID
+      deleteContactID: ID,
     }, () => {
       this.deleteContact();
     });
-  }
+  };
 
   blockContactHandler = (ID) => {
     this.setState({
       isLoading: true,
-      blockContactID: ID
+      blockContactID: ID,
     }, () => {
       this.blockContact();
     });
+  };
+
+  async deleteContact() {
+    return fetch(`http://localhost:3333/api/1.0.0/user/${this.state.deleteContactID}/contact`, {
+      method: 'delete',
+      headers: {
+        'x-authorization': await AsyncStorage.getItem('SessionToken'),
+      },
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          return;
+        }
+        if (response.status === 400) {
+          throw new Error('You cannot remove yourself as a contact');
+        }
+        if (response.status === 401) {
+          // User is unauthorised - return to login screen
+          this.props.navigation.navigate('Login');
+        }
+        if (response.status === 403) {
+          const err = 'You do not have the correct privilages to perform this action!';
+          throw err;
+        } else {
+          const err = 'Server Error! Please try again later!';
+          throw err;
+        }
+      })
+      .then(() => {
+        this.setState({
+          isLoading: false,
+          errorMessage: 'Contact Removed',
+        });
+        this.getData();
+      })
+      .catch((error) => {
+        this.setState({ errorMessage: error });
+      });
   }
 
-  styles = StyleSheet.create({
-    header:{
-      backgroundColor: '#3a75b5',
-      padding: 10,
-      color: 'white',
-      fontWeight: 'bold',
-      fontSize: 20
-    },
-    search: {
-      placeholderTextColor: 'gray',
-      backgroundColor: '#e8e5e3'
-    },
-    userButton: {
-      backgroundColor: '#3a75b5',
-      padding: 10,
-      width: 100
-    },
-    buttonText: {
-      color: 'white',
-      fontWeight: 'bold',
-      textAlign: 'center'
-    },
-    userContainer:{
-      display: 'flex',
-      justifyContent: "flex-start",
-      flexDirection: 'row',
-      flexWrap: 'wrap'
-    }
-  })
+  async blockContact() {
+    return fetch(`http://localhost:3333/api/1.0.0/user/${this.state.blockContactID}/block`, {
+      method: 'post',
+      headers: {
+        'x-authorization': await AsyncStorage.getItem('SessionToken'),
+      },
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          return;
+        }
+        if (response.status === 400) {
+          const err = 'You cannot block yourself!';
+          throw err;
+        }
+        if (response.status === 401) {
+          // User is unauthorised - return to login screen
+          this.props.navigation.navigate('Login');
+        }
+        if (response.status === 403) {
+          const err = 'You do not have the correct privilages to perform this action!';
+          throw err;
+        } else {
+          const err = 'Server Error! Please try again later!';
+          throw err;
+        }
+      })
+      .then(() => {
+        this.setState({
+          isLoading: false,
+          errorMessage: 'Contact Blocked',
+        });
+        this.getData();
+      })
+      .catch((error) => {
+        this.setState({ errorMessage: error });
+      });
+  }
+
   render() {
-    if (this.state.isLoading) {
+    const { state } = this;
+    if (state.isLoading) {
       return (
         <View>
           <ActivityIndicator />
         </View>
       );
     }
-    if(this.state.contactsListData.length ==0){
-      return(
-        <View>
-           <Text>No Current Contacts!</Text>
-        </View>
-      )
-    }
-    else {
+    if (state.contactsListData.length === 0) {
       return (
         <View>
-          <Text>{this.state.errorMessage}</Text>
-          <FlatList
-            data={this.state.contactsListData}
-            renderItem={({ item }) => (
-              <View>
-                <br/>
-                <View>
-                  <Text>{item.first_name} {item.last_name}</Text>
-                  <Text>{item.email}</Text>
-                </View>
-                <View>
-                  <TouchableOpacity style={this.styles.userButton} onPress={()=>this.deleteContactHandler(item.user_id)}>
-                    <Text style={this.styles.buttonText}>Remove Contact</Text>
-                  </TouchableOpacity>
-                  <br/>
-                  <TouchableOpacity style={this.styles.userButton} onPress={()=>this.blockContactHandler(item.user_id)}>
-                    <Text style={this.styles.buttonText}>Block User</Text>
-                  </TouchableOpacity>
-                </View>
-              </View>
-            )}
-            keyExtractor={({ user_id }, index) => user_id} />
+          <Text>No Current Contacts!</Text>
         </View>
-      )
+      );
     }
+
+    return (
+      <View>
+        <Text>{state.errorMessage}</Text>
+        <FlatList
+          data={state.contactsListData}
+          renderItem={({ item }) => (
+            <ListItem bottomDivider>
+              <Avatar
+                rounded
+                source={{ uri: '../Images/default.jpeg' }}
+              />
+              <ListItem.Content>
+                <ListItem.Title>
+                  {item.given_name}
+                  {' '}
+                  {item.family_name}
+                </ListItem.Title>
+                <ListItem.Subtitle>{item.email}</ListItem.Subtitle>
+              </ListItem.Content>
+              <View style={{
+                display: 'flex', justifyContent: 'center', flexDirection: 'row',
+              }}
+              >
+                <TouchableOpacity
+                  onPress={() => this.deleteContactHandler(item.user_id)}
+                >
+                  <Ionicons name="person-remove-outline" size="large" />
+                </TouchableOpacity>
+                <br />
+                <TouchableOpacity
+                  onPress={() => this.blockContactHandler(item.user_id)}
+                >
+                  <Ionicons name="eye-off-outline" size="large" />
+                </TouchableOpacity>
+              </View>
+            </ListItem>
+          )}
+          keyExtractor={({ userId }) => userId}
+        />
+      </View>
+    );
   }
 }
 
-export default Contacts
+export default Contacts;
